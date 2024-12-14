25021d93efc71f50c4f08db18c2e7e16d47a536d
LEAAKC <laboratorioensayosdeaguas@gmail.com>
2024-12-13 22:14:24 -0500

    configuracion del cors

--- src/index.js
+++ src/index.js
@@ -17,10 +17,9 @@ dotenv.config();
 //static files
 app.use(express.static('public'))
 //middleware
-app.use(cors()); //comunica la api con el servidor y ciertos dominios
+//app.use(cors()); //comunica la api con el servidor y ciertos dominios
 app.use(morgan('dev'))
 app.use(express.json()); //* middleware- analiza solicitudes entrantes con cargas JSON y se basa en body-parser
-
 
 const corsOptions = {
     origin: ['http://127.0.0.1:5173', 'https://leaakcsgmrc.netlify.app'],
