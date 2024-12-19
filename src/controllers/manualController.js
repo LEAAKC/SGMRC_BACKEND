440e9c8c0926153c3e0c24a09ed53d2ae54a36da
LEAAKC <laboratorioensayosdeaguas@gmail.com>
2024-12-18 23:16:28 -0500

    ajsute exportacion controlador downloadPdf

--- src/controllers/manualController.js
+++ src/controllers/manualController.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
 // Función para manejar la descarga del archivo PDF
-exports.downloadPdf = (req, res) => {
+export const downloadPdf = (req, res) => {
   const filePath = path.join(__dirname, '..', 'public', 'pdfFiles', 'manualAPPLABAKC.pdf');
   
   res.download(filePath, 'manualAPPLABAKC.pdf', (err) => {
