b090456764148e9fa823fd6566b846a473d69374
LEAAKC <laboratorioensayosdeaguas@gmail.com>
2024-12-18 23:46:23 -0500

    modificacion en el controlador para enviar y responder con el archivo y procesarlo en el frontend

--- src/controllers/manualController.js
+++ src/controllers/manualController.js
@@ -1,27 +1,24 @@
 import fs from 'fs';
 import path from 'path';
 
-// Función para manejar la descarga del archivo PDF
 export const downloadManual = (req, res) => {
   const filePath = path.join(process.cwd(), 'public', 'pdfFiles', 'manualAPPLABAKC.pdf');
-  
-  // Verificar si la ruta está bien construida
-  console.log('File Path:', filePath);
 
   // Verificar si el archivo existe
-  fs.exists(filePath, (exists) => {
-    if (!exists) {
-      console.error('El archivo no existe en la ruta especificada');
-      return res.status(404).send('El archivo no se encuentra disponible.');
-    }
+  if (!fs.existsSync(filePath)) {
+    console.error('El archivo no existe en la ruta especificada');
+    return res.status(404).send('El archivo no se encuentra disponible.');
+  }
 
-    // Si el archivo existe, proceder con la descarga
-    res.download(filePath, 'manualAPPLABAKC.pdf', (err) => {
-      if (err) {
-        console.error('Error al intentar descargar el archivo:', err);
-        return res.status(500).send('Error al intentar descargar el archivo.');
-      }
-      res.status(200);  // Opcional, ya que 'res.download' ya maneja el estado
-    });
+  // Establecer los encabezados para forzar la descarga
+  res.setHeader('Content-Disposition', 'attachment; filename=manualAPPLABAKC.pdf');
+  res.setHeader('Content-Type', 'application/pdf');
+
+  // Intentar enviar el archivo con sendFile
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error('Error al intentar enviar el archivo:', err);
+      return res.status(500).send('Error al intentar enviar el archivo.');
+    }
   });
 };
