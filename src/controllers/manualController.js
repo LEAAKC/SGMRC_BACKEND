2ef19f0cbc6a027db6f93ae4801763f84a81d2d7
LEAAKC <laboratorioensayosdeaguas@gmail.com>
2024-12-18 23:43:23 -0500

    nuevo intento cambio en controlador manual

--- src/controllers/manualController.js
+++ src/controllers/manualController.js
@@ -1,19 +1,27 @@
+import fs from 'fs';
 import path from 'path';
 
 // Función para manejar la descarga del archivo PDF
 export const downloadManual = (req, res) => {
-  // Obtener la ruta del archivo correctamente
   const filePath = path.join(process.cwd(), 'public', 'pdfFiles', 'manualAPPLABAKC.pdf');
   
   // Verificar si la ruta está bien construida
   console.log('File Path:', filePath);
 
-  // Intentar descargar el archivo
-  res.download(filePath, 'manualAPPLABAKC.pdf', (err) => {
-    if (err) {
-      console.error('Error al intentar descargar el archivo:', err);
-      return res.status(500).send('Error al intentar descargar el archivo.');
+  // Verificar si el archivo existe
+  fs.exists(filePath, (exists) => {
+    if (!exists) {
+      console.error('El archivo no existe en la ruta especificada');
+      return res.status(404).send('El archivo no se encuentra disponible.');
     }
-    res.status(200);  // Opcional, ya que 'res.download' ya maneja el estado
+
+    // Si el archivo existe, proceder con la descarga
+    res.download(filePath, 'manualAPPLABAKC.pdf', (err) => {
+      if (err) {
+        console.error('Error al intentar descargar el archivo:', err);
+        return res.status(500).send('Error al intentar descargar el archivo.');
+      }
+      res.status(200);  // Opcional, ya que 'res.download' ya maneja el estado
+    });
   });
 };
